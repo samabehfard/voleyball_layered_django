4661644ab6efdc7c3ff635734a93113421880c27
sama <raphichigo@gmail.com>
2024-09-21 20:25:39 +0330

    FIX: add identity_number being unique

--- user/models.py
+++ user/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     phone_number = models.CharField(max_length=20)
-    identity_number = models.CharField(max_length=50)
+    identity_number = models.CharField(max_length=50,unique=True,)
     name = models.CharField(max_length=50)
     family_name = models.CharField(max_length=50)
 
