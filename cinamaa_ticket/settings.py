1f785987d3ae3251977482499ba98c4a6a1febb6
sama <raphichigo@gmail.com>
2024-09-21 19:50:53 +0330

    ADD: add requirements file and their setting in setting

--- cinamaa_ticket/settings.py
+++ cinamaa_ticket/settings.py
@@ -126,3 +126,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Stadium Booking API',
+    'DESCRIPTION': 'API documentation for the stadium booking system.',
+    'VERSION': '1.0.0',
+    # Optional other settings
+}
+
+AUTH_USER_MODEL = "user.models.User"
